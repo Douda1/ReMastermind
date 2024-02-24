1395a73605a8e1861a84595ef8faed207cf556d8
Douda1 <160403454+Douda1@users.noreply.github.com>
2024-02-24 20:39:52 +0400

    Rajout des autres input

--- Remastermind.py
+++ Remastermind.py
@@ -17,13 +17,14 @@ Code3 = random.choice(liste)
 Code4 = random.choice(liste)
 CodeComplete = [Code1] + [Code2] + [Code3] + [Code4]
 Completion = [0, 0, 0, 0]
+CodeNumber = 0
 print(CodeComplete) # a retirer plus tard
 #----------------------------------------------------   
 
                     
 #----------------------------------------------------
-decaY = 40
-decaX = 0
+decaY = -15
+decaX = -100
 
 + decaY
 
@@ -51,19 +52,32 @@ class Button():
         self.appui = False
         
     def draw(self):
+        global CodeNumber
         pos = pygame.mouse.get_pos()
         if self.rect.collidepoint(pos):
             if pygame.mouse.get_pressed()[0] == 1 and self.appui == False:
                 self.appui = True
-                print('CLICKED')
+                Completion[CodeNumber] = self.val
+                CodeNumber += 1
+                print(Completion)
+                if CodeNumber == 4:
+                    CodeNumber = 0
+                    
         screen.blit(self.image, (self.rect.x, self.rect.y))
         
         if pygame.mouse.get_pressed()[0] == 0:
             self.appui = False
         
+        
 Bouton1_ = Button(275+decaX,600+decaY,Bouton1, 1)
-
-
+Bouton2_ = Button(500+decaX,600+decaY,Bouton2, 2)
+Bouton3_ = Button(725+decaX,600+decaY,Bouton3, 3)
+Bouton4_ = Button(275+decaX,450+decaY,Bouton4, 4)
+Bouton5_ = Button(500+decaX,450+decaY,Bouton5, 5)
+Bouton6_ = Button(725+decaX,450+decaY,Bouton6, 6)
+Bouton7_ = Button(275+decaX,300+decaY,Bouton7, 7)
+Bouton8_ = Button(500+decaX,300+decaY,Bouton8, 8)
+Bouton9_ = Button(725+decaX,300+decaY,Bouton9, 9)
 
 while True:
     for event in pygame.event.get():
@@ -80,5 +94,13 @@ while True:
     screen.blit(Fond_Gris,Fond_Gris_Rect)
     screen.blit(Fond_Noir,Fond_Noir_Rect)
     Bouton1_.draw()
+    Bouton2_.draw()
+    Bouton3_.draw()
+    Bouton4_.draw()
+    Bouton5_.draw()
+    Bouton6_.draw()
+    Bouton7_.draw()
+    Bouton8_.draw()
+    Bouton9_.draw()
     pygame.display.update()
     clock.tick(60)
